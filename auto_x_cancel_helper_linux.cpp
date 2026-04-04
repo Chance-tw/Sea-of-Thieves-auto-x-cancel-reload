8a4704aa863bd4ef16f4648c88e8cd8ec395d3d3
Chance <dustoffchopper.chance@gmail.com>
2026-04-04 11:56:10 -0400

    Add files via upload

---
+++ auto_x_cancel_helper_linux.cpp
@@ -0,0 +1,46 @@
+//compile with g++ auto_x_cancel_helper_linux.cpp -o auto_x_cancel_helper_linux -lX11
+#include <iostream>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <thread>
+#include <chrono>
+
+int main(){
+    Display *display=XOpenDisplay(NULL); //opens connection to display at the DISPLAY env var
+    if (display==NULL){
+        std::cout<<"Could not connect to the X server at "<<getenv("DISPLAY")<<"!\n";
+        return 0;
+    }
+    
+    Drawable root = DefaultRootWindow(display); //make a drawable on the root window
+
+    std::cout<<"Move your mouse tip to the right most bullet's center. 10 seconds until capture!\n";
+    std::this_thread::sleep_for(std::chrono::seconds(10));
+
+    //Gets the mouse's pixel position
+    Window root_return, child_return;
+    int root_x_return, root_y_return, win_x_return, win_y_return;
+    unsigned int mousebutton=0;
+    XQueryPointer(display, root, &root_return, &child_return, &root_x_return, &root_y_return, &win_x_return, &win_y_return, &mousebutton);
+    
+    XImage *captured=XGetImage(display, root, root_x_return, root_y_return, 1, 1, AllPlanes, ZPixmap);
+    /*
+    display: Specifies the connection to the X server.
+    root: Specifies where the drawable/window will be
+    root_x_return, root_y_return: (x, y) of the top left of rectangle to capture
+    1, 1: the size of the rectangle
+    AllPlanes: the pixel mask constant to take all the screen
+    ZPixmap: the format to return of the pixel map
+    */
+
+    //gets the color as decimal
+    unsigned long color=XGetPixel(captured, 0, 0);
+
+    std::cout<<"xcord is: "<<root_x_return<<", ycord is: "<<root_y_return<<'\n';
+    std::cout<<"rgbneed is: "<<color;
+
+    XDestroyImage(captured); //Destroyes the XImage
+    XCloseDisplay(display); //closes the connection to the X server
+
+    return 0;
+}
