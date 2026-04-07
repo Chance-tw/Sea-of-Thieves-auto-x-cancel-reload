f3a91fa1558313de5dd94509de1cd26e35247130
Chance <dustoffchopper.chance@gmail.com>
2026-04-07 00:18:52 -0400

    auto_x_cancel_helper_linux.cpp

--- auto_x_cancel_helper_linux.cpp
+++ auto_x_cancel_helper_linux.cpp
@@ -4,6 +4,14 @@
 #include <X11/Xutil.h>
 #include <thread>
 #include <chrono>
+#include <fstream>
+#include <filesystem>
+
+struct bulletvars{
+    int xcord=0;
+    int ycord=0;
+    std::uint32_t rgbneed=0;
+};
 
 int main(){
     Display *display=XOpenDisplay(NULL); //opens connection to display at the DISPLAY env var
@@ -11,10 +19,11 @@ int main(){
         std::cout<<"Could not connect to the X server at "<<getenv("DISPLAY")<<"!\n";
         return 0;
     }
-    
+
     Drawable root = DefaultRootWindow(display); //make a drawable on the root window
 
     std::cout<<"Move your mouse tip to the right most bullet's center. 10 seconds until capture!\n";
+    std::cout<<"Ensure the screenshot is fullscreen!\n";
     std::this_thread::sleep_for(std::chrono::seconds(10));
 
     //Gets the mouse's pixel position
@@ -37,10 +46,39 @@ int main(){
     unsigned long color=XGetPixel(captured, 0, 0);
 
     std::cout<<"xcord is: "<<root_x_return<<", ycord is: "<<root_y_return<<'\n';
-    std::cout<<"rgbneed is: "<<color;
+    std::cout<<"rgbneed is: "<<color<<'\n';
 
     XDestroyImage(captured); //Destroyes the XImage
     XCloseDisplay(display); //closes the connection to the X server
+
+    //populate a struct mirroring the bulletvars struct of the actual macro
+    bulletvars cfgfile;
+    cfgfile.xcord=root_x_return;
+    cfgfile.ycord=root_y_return;
+    cfgfile.rgbneed=color;
+
+    //prep for new destination stuff
+    std::string newdirpath=std::string(getenv("HOME"))+"/.config/auto_x_cancel";
+    std::string newfilepath=newdirpath+"/bullet-config";
+
+    try{
+        std::filesystem::create_directories(newdirpath);
+    }
+    catch(const std::filesystem::filesystem_error& e){ //should catch any errors with writing to the filesystem, but idk how to intentionally cause file system errors
+        std::cout<<"Failed to create program directory at "<<newdirpath<<'\n';
+        return 0;
+    }
+
+    //open/create file to write
+    std::ofstream bulletcfg{newfilepath, std::ios::binary};
+    if (!bulletcfg){
+        std::cout<<"Failed to write to "<<newdirpath<<"/bullet-config\n";
+        return 0;
+    }
+
+    //write the binary and close
+    bulletcfg.write(reinterpret_cast<const char*>(&cfgfile), sizeof(cfgfile));
+    bulletcfg.close();
 
     return 0;
 }
